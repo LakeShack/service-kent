183e0cedb2a1991bf4b408f92bba11d2461c8480
Kent Wang <kentwang@Kents-MacBook-Pro.local>
2018-04-17 09:48:21 -0700

    fixed testing names

--- test/HomePage.react.test.js
+++ test/HomePage.react.test.js
@@ -11,11 +11,11 @@ import Introduction from '../client/src/components/Introduction.jsx';
 describe('homepage rendered', () => {
  
 //test rendering of the home page   
-  it('should render show HomePage class as home-page', () => {
+  it('HomePage component should render the home-page div', () => {
     expect(shallow(<HomePage />).is('.home-page')).toBe(true);
   });
 
-  it('should mount in a full DOM', function() {
+  it('should mount only 1', function() {
     expect(mount(<HomePage />).find('.home-page').length).toBe(1);
   });
 
